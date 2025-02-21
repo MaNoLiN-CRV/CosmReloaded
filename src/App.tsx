62fe8e673b284aa3a5252a30a6f72e615a914912
MaNoLiN-CRV <manoliin.27@gmail.com>
2025-02-21 14:01:37 +0100

    People component implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@
 import './App.css'
 import People from './components/People'
 
+
+const team = [
+  {
+    name: 'Félix Caba',
+    role: 'Founder, Full-Stack Developer',
+    image: 'felix.jpg'
+  },  
+  {
+    name: 'Manuel Cervantes',
+    role : 'Co-Founder, Full-Stack Developer',
+    image : 'manu.jpg'
+  }
+] 
+
 function App() {
   return (
     <>
      
-      <People />
+      <People people={team}/>
     </>
   )
 }
--- src/components/People.tsx
+++ src/components/People.tsx
@@ -1,44 +1,39 @@
-const people = [
-  {
-    name: 'Leslie Alexander',
-    role: 'Co-Founder / CEO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  
-  {
-    name: 'Hedy Lamarr',
-    role: 'Co-Founder',
-    imageUrl: 'https://images.unsplash.com/photo-1550525811-e5869dd03032?auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  // More people...
-]
+export interface Person {
+  name: string;
+  role: string;
+  image?: string;
+}
 
-export default function People() {
+interface PeopleProps {
+  people: Person[];
+}
+
+export default function People({ people }: PeopleProps) {
   return (
-    <div className="bg-gradient-to-b from-blue-400 to-black py-12 sm:py-16 transform -skew-y-3 rounded-lg">
+    <div className="relative bg-gradient-to-b from-[#ffa07a] to-[#242424] py-12 sm:py-16 transform -skew-y-3 rounded-lg">
       <div className="container mx-auto max-w-6xl p-10 xl:grid xl:grid-cols-3 xl:gap-10">
         <div className="xl:col-span-1">
-          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            Meet Our Leadership
+          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl [text-shadow:0.5px_1px_2px_black]">Meet the Team
           </h2>
-          <p className="mt-6 text-lg text-gray-300">
-            We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients.
+          <p className="mt-6 text-lg text-gray-300 font-mono [text-shadow:0.5px_0.5px_1px_black]">
+            We are a small team of developers, designers, and creators. We are passionate about what we do and are always looking for new challenges.
           </p>
         </div>
-        <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2 xl:gap-x-10">
-          {people.map((person) => (
-            <li key={person.name} className="transition-transform duration-300 hover:scale-105">
-              <div className="flex items-center gap-x-6 p-4 bg-gray-800 rounded-lg shadow-lg">
-                <img alt={person.name} src={person.imageUrl} className="rounded-full h-16 w-16" />
-                <div>
-                  <h3 className="text-lg font-semibold text-white">{person.name}</h3>
-                  <p className="text-sm text-indigo-500">{person.role}</p>
+        <div className="xl:col-span-2">
+          <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16">
+            {people.map((person) => (
+              <li key={person.name} className="transition-transform duration-300 hover:scale-105">
+                <div className="flex items-center gap-x-6 p-4 bg-gray-800 bg-opacity-60 backdrop-filter backdrop-blur-lg backdrop-saturate-200 rounded-lg shadow-lg">
+                  {person.image && <img alt={person.name} src={`/${person.image}`} className="rounded-full h-16 w-16 object-cover" />}
+                  <div>
+                    <h3 className="text-lg font-semibold text-white">{person.name}</h3>
+                    <p className="text-sm text-gray-400">{person.role}</p>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
